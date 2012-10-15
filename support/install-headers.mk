dec7baaf0cee77123f214b7d541cf7b4d2ef8527
Donald Kirker <donald.kirker@openmobl.com>
2012-10-15 08:55:53 +0000

    Add new Makefiles.

--- support/install-headers.mk
+++ support/install-headers.mk
@@ -7,5 +7,5 @@ staging/armv7/usr/include/.staged: staging/$(WOCE_ARCH)/.unpacked $(BUILD_SUPPOR
 	mkdir -p staging/armv7/usr/include
 	tar -C staging/armv7/usr/include --strip=4 --wildcards -axf $(BUILD_SUPPORT_DL) 'woce-build-support-[a-f0-9]*/staging/arm-none-linux-gnueabi/include'
 	tar -C staging/armv7/usr/lib --strip=4 --wildcards -axf $(BUILD_SUPPORT_DL) 'woce-build-support-[a-f0-9]*/staging/arm-none-linux-gnueabi/lib'
-	ln -s staging/armv7/usr/lib/libsqlite3.so.0 staging/armv7/usr/lib/libsqlite3.so
+	ln -s libsqlite3.so.0 staging/armv7/usr/lib/libsqlite3.so
 	touch $@
